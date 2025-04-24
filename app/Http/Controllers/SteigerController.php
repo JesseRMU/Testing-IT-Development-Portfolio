091f25002db6646513c580aece155d3b12776811
DragonFlight01 <54706501+DragonFlight01@users.noreply.github.com>
2025-04-24 11:42:02 +0200

    Added migrations + object files

---
+++ app/Http/Controllers/SteigerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Steiger;
+use Illuminate\Http\Request;
+
+class SteigerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Steiger $steiger)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Steiger $steiger)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Steiger $steiger)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Steiger $steiger)
+    {
+        //
+    }
+}
---
+++ database/migrations/0001_01_01_000006_create_evenementen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evenementen', function (Blueprint $table) {
+            $table->id('evenement_id');
+            $table->foreignId('schip_id');
+            $table->foreignId('object_id')->nullable();
+            $table->foreignId('steiger_id')->nullable();
+            $table->datetime('evenement_begin_datum')->nullable();
+            $table->datetime('evenement_eind_datum')->nullable();
+            $table->string('evenement_vaarrichting')->nullable();
+
+            $table->foreign('schip_id')->references('schip_id')->on('schepen');
+            $table->foreign('object_id')->references('object_id')->on('objecten');
+            $table->foreign('steiger_id')->references('steiger_id')->on('steigers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evenementen');
+    }
+};

5250018aa58083551b9fe494005f3fd09d015e73
Jesse <96941057+JesseRMU@users.noreply.github.com>
2025-05-20 18:19:04 +0200

    heatmap toont drukte per steiger

--- app/Http/Controllers/HeatmapController.php
+++ app/Http/Controllers/HeatmapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evenement;
 use Illuminate\Http\Request;
 use App\Models\Steiger;
 
@@ -9,12 +10,18 @@ class HeatmapController extends Controller
 {
     public function index()
     {
-        // Haal alle steigers op waar lat en lng zijn ingevuld
-        $steigers = Steiger::whereNotNull('latitude')
-            ->whereNotNull('longitude')
-            ->get(['steiger_code', 'latitude', 'longitude']);
-
-        // Stuur data naar view
-        return view('heatmap.index', compact('steigers'));
+        $coordinates = Evenement::with('steiger')
+            ->whereHas('steiger', function ($query) {
+                $query->whereNotNull('latitude')->whereNotNull('longitude');
+            })
+            ->get()
+            ->map(function ($evenement) {
+                return [
+                    $evenement->steiger->latitude,
+                    $evenement->steiger->longitude,
+                    0.004, // heatmap intensiteit per punt
+                ];
+            });
+        return view('heatmap.index', compact('coordinates'));
     }
 }
--- resources/views/heatmap/index.blade.php
+++ resources/views/heatmap/index.blade.php
@@ -1,40 +1,33 @@
+<x-main title="Heatmap">
+    <div id="map" style="height: 600px;"></div>
 
-    <x-main title="Heatmap">
-        <div id="map" style="height: 600px;"></div>
+    <!-- Leaflet CSS en JS, plus leaflet.heat plugin -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"></script>
 
-        <!-- Leaflet CSS en JS, plus leaflet.heat plugin -->
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-        <script src="https://unpkg.com/leaflet.heat/dist/leaflet-heat.js"></script>
+    <script>
+        // Zet de kaart op Zeeland
+        const map = L.map('map').setView([51.5, 3.9], 11);
 
-        <script>
-            // Zet de kaart neer op Zeeland
-            const map = L.map('map').setView([51.5, 3.9], 11);
+        // Voeg OpenStreetMap tiles toe
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+        }).addTo(map);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 18,
-            }).addTo(map);
+        // Haal coördinaten met intensiteit uit Laravel
+        const coordinates = @json($coordinates);
+        console.log(coordinates); // check of alles klopt
 
-            // Haal steiger-coördinaten uit Laravel (komt uit de controller)
-            const steigers = @json($steigers);
-            console.log(steigers);
+        // Voeg heatlayer toe
+        L.heatLayer(coordinates, {
+            radius: 25,
+            blur: 15,
+            maxZoom: 17,
+            minOpacity: 0.2,
+            maxOpacity: 1
+        }).addTo(map);
+    </script>
 
-            // Zet om naar heatmap formaat: [lat, lng, intensity]
-            const heatPoints = steigers.map(steiger => [
-                steiger.latitude,
-                steiger.longitude,
-                1  // vaste intensiteit voor nu
-            ]);
-
-            // Voeg heatlayer toe
-            L.heatLayer(heatPoints, {
-                radius: 25,
-                blur: 15,
-                maxZoom: 17,
-                minOpacity: 0.5,
-                maxOpacity: 0.8
-            }).addTo(map);
-        </script>
-
-        <div class="h-15"></div>
-    </x-main>
+    <div class="h-15"></div>
+</x-main>

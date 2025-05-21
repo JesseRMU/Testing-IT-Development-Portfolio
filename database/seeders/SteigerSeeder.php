c58703a242cf0fcc3715476c5ebe6f83055d02c6
Jesse <96941057+JesseRMU@users.noreply.github.com>
2025-05-21 11:57:39 +0200

    added heatmap

--- database/seeders/SteigerSeeder.php
+++ database/seeders/SteigerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Steiger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class SteigerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $steigers = json_decode(file_get_contents('export steiger table.json'), false);
+        foreach ($steigers->rows as $steiger) {
+            $a = ["steiger_id" => $steiger[0],
+                "wachthaven_id" => $steiger[1],
+                "steiger_code" => $steiger[2],];
+            new Steiger($a);
+        }
     }
 }

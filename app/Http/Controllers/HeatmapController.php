25923a1bad406b0f41234962dad1ea22b8ee29b7
Adriaan Zwemer <zwem0022@hz.nl>
2025-05-21 14:08:38 +0200

    Verbeter heatmap
    Co-Authored-By: Jesse van Belzen <96941057+JesseRMU@users.noreply.github.com>

--- app/Http/Controllers/HeatmapController.php
+++ app/Http/Controllers/HeatmapController.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 use App\Models\Steiger;
+use Illuminate\Support\Facades\DB;
 
 class HeatmapController extends Controller
 {
@@ -16,18 +17,14 @@ class HeatmapController extends Controller
      */
     public function index()
     {
-        $coordinates = Evenement::with('steiger')
-            ->whereHas('steiger', function ($query) {
-                $query->whereNotNull('latitude')->whereNotNull('longitude');
-            })
-            ->get()
+        $coordinates = DB::table("steigers")->join("evenementen", "steigers.steiger_id", "=", "evenementen.steiger_id")->select(DB::raw("count(*) as hoeveelheid, longitude, latitude"))->whereNotNull("latitude")->groupBy("steigers.steiger_id")->get()
             ->map(function ($evenement) {
                 return [
-                    $evenement->steiger->latitude,
-                    $evenement->steiger->longitude,
-                    0.002, // heatmap intensiteit per punt
+                    $evenement->latitude,
+                    $evenement->longitude,
+                    $evenement->hoeveelheid, // heatmap intensiteit per punt
                 ];
-            });
+            });;
         return view('heatmap.index', compact('coordinates'));
     }
 }

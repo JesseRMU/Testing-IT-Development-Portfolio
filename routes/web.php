cb29e1f5bb32b31c89f5d942c99c369c8e595d96
Saeed <alha0013@hz.nl>
2025-06-19 11:47:30 +0200

    web.php aangepast

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,7 @@ Route::get("/filters", function () {
 Route::get('/heatmap', [HeatmapController::class, 'index'])->name("heatmap");
 
 // Route voor het herschikken van grafiekdata
- us6-verwijderen-foute-data
 Route::post('/chart/groupByTime', [EvenementController::class, 'groupByTime'])->name('chart.groupByTime');
 Route::delete('/evenementen/{id}', [EvenementController::class, 'destroy'])->name('evenementen.destroy');
 
 Route::get('/chart/groupByTime', [EvenementController::class, 'groupByTime'])->name('chart.groupByTime');
- main

8be22119e00a754a993171245f3410caa97f8f4b
Jesse <96941057+JesseRMU@users.noreply.github.com>
2025-06-26 14:58:06 +0200

    overbodige stukjes code weggehaald

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot()
+    public function boot(): void
     {
-
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,6 @@ use App\Http\Controllers\ExcelUpload;
 use App\Http\Controllers\EvenementController;
 use App\Http\Controllers\GraphController;
 
-
-
-Route::get('/testje', function () {
-    return 'werkt';
-});
-
-
 Route::get('/', function () {
     return view('index');
 })->name("home");
@@ -29,6 +22,5 @@ Route::get('/heatmap', [HeatmapController::class, 'index'])->name("heatmap");
 // Route voor het herschikken van grafiekdata
 Route::post('/chart/groupByTime', [EvenementController::class, 'groupByTime'])->name('chart.groupByTime');
 
-//Route::get('/evenementen/available-dates', [EvenementController::class, 'getAvailableDates']);
-
+// Route voor het ophalen van de datums die geactiveerd moeten worden
 Route::get('/api/evenementen/dates', [EvenementController::class, 'getAvailableDates']);
